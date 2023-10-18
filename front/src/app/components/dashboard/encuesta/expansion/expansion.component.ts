702e9bf561bfd75b4e656d8a604aec0d779a86fe
Semper Evincere <spereyra.jus@gmail.com>
2023-10-18 10:57:42 -0300

    logica para que no se visualicen algunas opciones referidas a la oficina de turismo, ventana de cambio de pass centrada

--- front/src/app/components/dashboard/encuesta/expansion/expansion.component.ts
+++ front/src/app/components/dashboard/encuesta/expansion/expansion.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, FormGroupName, ValidationErrors, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -8,14 +8,15 @@ import { environment } from 'src/environments/environment';
 import { SurveyList } from 'src/app/interfaces/SurveyList';
 import { UserService } from 'src/app/services/usuario.service'
 import { Observable, firstValueFrom, map, startWith } from 'rxjs';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 @Component({
   selector: 'app-expansion',
   templateUrl: './expansion.component.html',
   styleUrls: ['./expansion.component.css']
 })
 export class ExpansionComponent implements OnInit {
+  oficinaAsistio = true;
   setcheckbox = true;
-
   accordion: any;
   pasoUno: FormGroup;
   pregunta9: FormGroup;
@@ -37,11 +38,13 @@ export class ExpansionComponent implements OnInit {
   formEnviado: any;
 
   constructor(
+    public expandedQuestion: ElementRef,
     private fb: FormBuilder,
     private _snackBar: MatSnackBar,
     private http: HttpClient,
     private userService: UserService,
     private route: Router) {
+
 
     //pregunta 9
     this.pregunta9 = this.fb.group({
@@ -99,62 +102,64 @@ export class ExpansionComponent implements OnInit {
     });
 
     //pregunta 16
-    this.pregunta16 = this.fb.group({
-      rotonda: [false],
-      merlo: [false],
-      terminal: [false],
-    }, { validators: this.alMenosUna });
+      this.pregunta16 = this.fb.group({
+        rotonda: [false],
+        merlo: [false],
+        terminal: [false],
+      });
 
-    //pregunta 17
-    this.pregunta17 = this.fb.group({
-      hospedaje: [false],
-      paseos: [false],
-      eventos: [false],
-      gastronomia: [false],
-      aventura: [false],
-      servicios: [false],
-      rutas: [false],
-      otro: ['']
-    }, { validators: this.alMenosUna });
+      //pregunta 17
+      this.pregunta17 = this.fb.group({
+        hospedaje: [false],
+        paseos: [false],
+        eventos: [false],
+        gastronomia: [false],
+        aventura: [false],
+        servicios: [false],
+        rutas: [false],
+        otro: ['']
+      });
 
-    //pregunta 18
-    this.pregunta18 = this.fb.group({
-      personalmente: [false],
-      mail: [false],
-      facebook: [false],
-      telefono: [false],
-      otro: ['']
-    }, { validators: this.alMenosUna });
+      //pregunta 18
+      this.pregunta18 = this.fb.group({
+        personalmente: [false],
+        mail: [false],
+        facebook: [false],
+        telefono: [false],
+        otro: ['']
+      });
 
-    //pregunta 19
-    this.pregunta19 = this.fb.group({
-      folleto: [false],
-      revista: [false],
-      plano: [false],
-      calcomania: [false],
-      guia: [false]
-    }, { validators: this.alMenosUna });
+      //pregunta 19
+      this.pregunta19 = this.fb.group({
+        folleto: [false],
+        revista: [false],
+        plano: [false],
+        calcomania: [false],
+        guia: [false]
+      });
 
-    //pregunta 20
-    this.pregunta20 = this.fb.group({
-      calidadInformacion: [null, Validators.required],
-    });
+      //pregunta 20
+      this.pregunta20 = this.fb.group({
+        calidadInformacion: [null, Validators.required],
+      });
 
-    //pregunta 21
-    this.pregunta21 = this.fb.group({
-      otraInfo: [null, Validators.required]
-    });
+      //pregunta 21
+      this.pregunta21 = this.fb.group({
+        otraInfo: [null, Validators.required]
+      });
 
-    //pregunta 22
-    this.pregunta22 = this.fb.group({
-      espectaculo: [false],
-      espectaculoCercano: [false],
-      recreacion: [false],
-      deporte: [false],
-      aventura: [false],
-      paseo: [false],
-      otro: ['']
-    }, { validators: this.alMenosUna });
+      //pregunta 22
+      this.pregunta22 = this.fb.group({
+        espectaculo: [false],
+        espectaculoCercano: [false],
+        recreacion: [false],
+        deporte: [false],
+        aventura: [false],
+        paseo: [false],
+        otro: ['']
+      });
+    
+
 
     //pregunta 23
     this.pregunta23 = this.fb.group({
@@ -184,26 +189,33 @@ export class ExpansionComponent implements OnInit {
       pregunta13: this.pregunta13, // asignando el FormGroup pregunta13
       pregunta14: this.pregunta14, // asignando el FormGroup pregunta14
       pregunta15: this.pregunta15, // asignando el FormGroup pregunta15
-      pregunta16: this.pregunta16, // asignando el FormGroup pregunta16
-      pregunta17: this.pregunta17, // asignando el FormGroup pregunta17
-      pregunta18: this.pregunta18, // asignando el FormGroup pregunta18
-      pregunta19: this.pregunta19, // asignando el FormGroup pregunta19
-      pregunta20: this.pregunta20, // asignando el FormGroup pregunta20
-      pregunta21: this.pregunta21, // asignando el FormGroup pregunta21
-      pregunta22: this.pregunta22, // asignando el FormGroup pregunta22
+      // pregunta16: this.pregunta16, // asignando el FormGroup pregunta16
+      // pregunta17: this.pregunta17, // asignando el FormGroup pregunta17
+      // pregunta18: this.pregunta18, // asignando el FormGroup pregunta18
+      // pregunta19: this.pregunta19, // asignando el FormGroup pregunta19
+      // pregunta20: this.pregunta20, // asignando el FormGroup pregunta20
+      // pregunta21: this.pregunta21, // asignando el FormGroup pregunta21
+      // pregunta22: this.pregunta22, // asignando el FormGroup pregunta22
       pregunta23: this.pregunta23, // asignando el FormGroup pregunta23
       pregunta24: this.pregunta24, // asignando el FormGroup pregunta24
 
     });
   }
-  
+
+
   private appUrl = environment.APP_URL;
-  ngOnInit(): void { }
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    const inputElement = document.querySelector(`[formControlName="pregunta1"]`) as HTMLInputElement;
+    inputElement.focus();
+  }
 
   step = 0;
 
   nextStep(event: Event) {
-    event.preventDefault(); // Evita la acción predeterminada del botón
+    event.preventDefault();
     this.step++;
   }
 
@@ -295,4 +307,45 @@ export class ExpansionComponent implements OnInit {
       }
     }
   }
+
+  focusInputOnTabChange(event: MatTabChangeEvent): void {
+    const tab = event.tab.textLabel; // Obtenemos el nombre de la pestaña activa
+
+    // Definimos los campos de entrada correspondientes en cada pestaña
+    const inputFields: { [key: string]: string } = {
+      'Nombre': 'pregunta1',
+      'Email': 'pregunta2',
+      'Edad': 'pregunta3',
+      'Género': 'pregunta4',
+    };
+
+    // Obtenemos el nombre del campo de entrada
+    const inputName = inputFields[tab];
+
+
+    // Enfocamos el campo de entrada
+    if (inputName) {
+      const inputElement = document.querySelector(`[formControlName="${inputName}"]`) as HTMLInputElement;
+
+      if (inputElement) {
+        inputElement.focus();
+      }
+    }
+  }
+
+  onQuestionExpanded(expandedQuestion: ElementRef) {
+    expandedQuestion.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
+
+  onInformesChange() {
+    // this.oficinaAsistio = !this.oficinaAsistio;
+    const pregunta15Control = this.pasoUno.get('pregunta15');
+    if (pregunta15Control) {
+      const informesValue = pregunta15Control.value;
+      this.oficinaAsistio = informesValue === 'si';
+    } else {
+      return
+    }
+  }
+
 }
